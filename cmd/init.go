353e9d6e006e6c8948d8826237af6dfdbd55dd8f
Bruno Leite <bruno.marinho@useall.com.br>
2024-09-16 11:36:09 -0300

    fix functiosn

--- cmd/init.go
+++ cmd/init.go
@@ -1,18 +1,17 @@
 package cmd
 
 import (
-	"github.com/brunoleitem/gr/data"
 	"github.com/brunoleitem/gr/handler"
 	"github.com/spf13/cobra"
 )
 
 var initCmd = &cobra.Command{
-	Use:   "token",
+	Use:   "init",
 	Short: "",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		data.CreateTables()
-		handler.Init()
+		// data.CreateTables()
+		handler.Generate()
 	},
 }
 
--- handler/generate.go
+++ handler/generate.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/brunoleitem/gr/internal/prompt"
 )
@@ -11,6 +12,12 @@ func Generate() {
 	askContent := pc.Create("Please provide brief overview about your project!", "Overview: ")
 	content := pc.Get(*askContent)
 	fmt.Println(content)
+
+	cmd := exec.Command("git", "init")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
 	// client := openai.CreateClient()
 	// client.CreateReadme(content)
 }
